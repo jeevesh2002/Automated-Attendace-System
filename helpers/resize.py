1d97617dba09935eaed9902e341066084c89dd18
Jeevesh Krishna <a.jeeveshkrishna@gmail.com>
2021-06-23 21:41:54 +0530

    Add files via upload

--- helpers/resize.py
+++ helpers/resize.py
@@ -1,1 +1,14 @@
+import cv2
+import os
+def resize():
+    
+    path = input("Enter path of image directory")
+    for root, subdir, files in os.walk(path):
+        for file in files:
+            image_path = os.path.realpath(file)
+            image = cv2.imread(f"initial_assets/{file}",cv2.IMREAD_GRAYSCALE)
+            resized_image = cv2.resize(image, dsize=[250,250])
+            cv2.imwrite(f"final_assets/resized_{file}",resized_image)
+            
+
 
